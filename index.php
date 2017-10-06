a754781e0f16f538acc00c7899c5cab1ac05cc7f
Mike Donkers <donkers.mbj@gmail.com>
2017-10-06 10:01:59 +0200

    remove bullshit livereload

--- index.php
+++ index.php
@@ -199,6 +199,5 @@ if (isset($_SESSION['Logintoken']) && isset($_SESSION['UserID'])) {
 		}
 		?>
 	</main>
-	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 </body>
 </html>
