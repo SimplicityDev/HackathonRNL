a09df03141b23d37ffb07078d3db5b64c5c345a8
William Ross <wross@live.nl>
2017-10-07 00:45:24 +0200

    Home -> images, style Nav -> fixed

--- Portaal/src/app/home/home.component.ts
+++ Portaal/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from "../api.service";
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    public requests = [
+        {
+            title: "Dakkapel",
+            step: 3,
+            totalSteps: 12,
+            person: {
+                street: "plein",
+                postalCode: "1234AB",
+                city: "Eindhoven"
+            },
+            desc: "lorem ipsum dolor sit amet si;dughunaek;j gakldng asld knfbslkd jfng sldkjfn skdjf ksdfj nskdlf nsdkfj nsndmf nsd ,sdf ",
+            image: "dakkapel.jpg"
+        },{
+            title: "Steiger",
+            step: 2,
+            totalSteps: 8,
+            person: {
+                street: "plein",
+                postalCode: "1234AB",
+                city: "Eindhoven"
+            },
+            desc: "qwertyuiopasdf hjkl;zxcvbnm wertyuisdfghjvbn edfgbnjyhgvbnlouytgfc vbnhmnytghv",
+            image: "steiger.jpg"
+        }, {
+            title: "Huis slopen",
+            step: 3,
+            person: {
+                street: "plein",
+                postalCode: "1234AB",
+                city: "Eindhoven"
+            },
+            totalSteps: 4,
+            desc: "Sloop die shit",
+            image: "slopen.jpg"
+        },{
+            title: "Steiger",
+            step: 2,
+            totalSteps: 8,
+            person: {
+                street: "plein",
+                postalCode: "1234AB",
+                city: "Eindhoven"
+            },
+            desc: "qwertyuiopasdfghj kl;zxcvbnm wertyuisdfghjvbn edfgbnjyhgvbnlouytgfc vbnhmnytghv",
+            image: "steiger.jpg"
+        }
+    ];
+  constructor(ApiService: ApiService) {
+    ApiService.requests(localStorage.getItem("user_id"));
+  }
+
 
   ngOnInit() {
   }
